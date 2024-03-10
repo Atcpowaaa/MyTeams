3e7b60d23419f8317cc8e7db084294e6ab6043b5
Thomas Lardé <87072807+Pepsilacanette@users.noreply.github.com>
2024-03-10 20:26:03 +0100

    Update client.c

--- client.c
+++ client.c
@@ -1,72 +1,54 @@
-#include<stdio.h>
-#include<string.h>
-#include<sys/socket.h>
-#include<arpa/inet.h> // Adresse inet
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h> // Adresse inet
+#include <unistd.h>
 
 int main(int argc, char **argv) {
 
-    // Init avec la def du socket, le message, la reponse serveur, et la definition du srveur
-    
     int socket1;
-
     char* message = "test";
-
     char server_reply[2000];
-
     struct sockaddr_in server;
 
-    // Def du socket
-
-    socket1 = socket(AF_INET, SOCK_STREAM, 0); // AF_INET = IPV4 | SOCK_STREAM = TCP
+    // Création du socket
+    socket1 = socket(AF_INET, SOCK_STREAM, 0); // AF_INET = IPv4, SOCK_STREAM = TCP
 
     if (socket1 == -1) {
-
         printf("Impossible de créer le socket");
-
+        return 1;
     }
 
-    return 0;
-
-
-    // Def du port, à changer si serveur change, sachant que le premier c'est l'IP, deuxième l'IPV4 et le troisème le port
-
+    // Définition du port et connexion au serveur
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
     server.sin_port = htons(4242);
 
-    if (connect(socket1, (struct sockaddr * )&server, sizeof(server)) < 0) {
-        
-        puts("Problème de connection");
+    if (connect(socket1, (struct sockaddr *)&server, sizeof(server)) < 0) {
+        puts("Problème de connexion");
         return 1;
-
     }
 
     puts("Connecté");
-    return 0;
 
-    // Envoie du message
-
+    // Envoi du message
     message = "GET / HTTP/1.1\r\n\r\n";
     if (send(socket1, message, strlen(message), 0) < 0) {
-
-        puts("Erreur pendant l'envoie du mesage\n");
+        puts("Erreur pendant l'envoi du message\n");
         return 1;
-
     }
 
-    // Réponse du serveur ici
-
+    // Réception de la réponse du serveur
     if (recv(socket1, server_reply, 2000, 0) < 0) {
-
         puts("Erreur de la réponse serveur");
-
+        return 1;
     }
 
-    puts("Réponse reçu\n");
+    puts("Réponse reçue\n");
     puts(server_reply);
 
-    puts("Message envoyé\n");
-
+    // Fermeture du socket
     close(socket1);
+
     return 0;
 }
